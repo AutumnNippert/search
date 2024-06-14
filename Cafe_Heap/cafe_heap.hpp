b9d1a7dea6621d796f92ffeec6fae9d4e06993c5
Devin Thomas <devin.w.thomas@gmail.com>
2024-06-14 15:38:05 -0400

    cpp - progress

--- Cafe_Heap/cafe_heap.hpp
+++ Cafe_Heap/cafe_heap.hpp
@@ -1,18 +1,17 @@
 #include <cassert>
 #include <boost/heap/d_ary_heap.hpp>
 #include "flagged_ptr.hpp"
+#include "ordered_circular_queue.hpp"
 
-template <class Node_t>
+template <typename Node_t, typename Compare>
 class Cafe_Heap{
     private:
         using Queue = boost::heap::d_ary_heap<Node_t *, 
                                 boost::heap::arity<2>, 
                                 boost::heap::mutable_<true>,
-                                boost::heap::compare<std::greater<Node_t *>>>;
-        Flagged_Pointer<Node_t *> * _head_queue;
-        std::size_t _head_capacity
+                                boost::heap::compare<flagged_ptr_cmp<Node_t *, Compare>>>;
+        Ordered_Circular_Queue<Flagged_Pointer<Node_t *>, flagged_ptr_cmp<Node_t *, Compare>> _head_queue;
         Queue _tail_queue;
     public:
-
-
+        
 };
