3fa8fb65542f6a9f772734d3cf537431f35b0ba3
Devin Wild Thomas <dwt@cs.unh.edu>
2024-07-09 15:00:41 -0400

    cpp - implementing

--- Cafe_Heap/cafe_heap.hpp
+++ Cafe_Heap/cafe_heap.hpp
@@ -1,39 +1,152 @@
 #include <cassert>
-#include <boost/heap/d_ary_heap.hpp>
-#include "flagged_ptr.hpp"
-#include "ordered_circular_queue.hpp"
+#include <memory>
+
+using handle_t = std::size_t;
+
+struct PrecomputeFlags{
+    bool reserved;
+    bool completed;
+
+    constexpr PrecomputeFlags():reserved(false),completed(false){}
+
+    static constexpr PrecomputeFlags both_false(){
+        PrecomputeFlags retval(false, false);
+        return retval;
+    }
+};
+
 
 template <typename Node_t, typename Compare>
-class Cafe_Heap{
+struct HeapNode{
+    Node_t search_node;  // the usual search node: {state, g, f, parent...}
+    Node_t[] precomputed_successors;  // array of precomputed successors 
+    handle_t handle;
+    std::atomic<PrecomputeFlags> flags; // precomputation flags
+
+    constexpr HeapNode(const Node_t& node, handle_t h):search_node(node),precomputed_successors(nullptr),handle(h){
+        flags.store(PrecomputeFlags::both_false(), std::memory_order_release);
+    };
+    
+    constexpr ~HeapNode(){
+        if(precomputed_successors != nullptr){
+            delete[] precomputed_successors;
+        }
+    }
+
+
+
+};
+
+constexpr handle_t parent(handle_t i) const{
+    if (i == 0){
+        return 0;
+    }
+    return (i - 1)/2;
+}
+
+constexpr handle_t left_child(handle_t i) const{
+    return 2*i + 1;
+}
+
+constexpr handle_t right_child(handle_t i) const{
+    return 2*i + 2;
+}
+
+template <typename Node_t, typename Compare>
+inline void swap_helper(i, j, HeapNode<Node_t, Compare> * xi, HeapNode<Node_t, Compare> * xj, std::atomic<HeapNode<Node_t, Compare> *> dat_arr[]){
+    std::swap(xi->handle, xj->handle);                  // workers don't get to touch handles
+    dat_arr[i].store(xj, std::memory_order_relaxed);    // We don't care if two workers see the same pointer twice
+    dat_arr[j].store(xi, std::memory_order_relaxed);    // swap conserves the number of elements
+}
+
+
+template <typename Node_t, typename Compare>
+class CafeMinBinaryHeap{
     private:
-        using Queue = boost::heap::d_ary_heap<Flagged_Pointer<Node_t *>, 
-                                boost::heap::arity<2>, 
-                                boost::heap::mutable_<true>,
-                                boost::heap::compare<flagged_ptr_cmp<Node_t *, Compare>>>;
-        Ordered_Circular_Queue<Flagged_Pointer<Node_t *>, flagged_ptr_cmp<Node_t *, Compare>> _head_queue;
-        Queue _tail_queue;
+        std::atomic<HeapNode<Node_t, Compare> *> _data[]; // of atomic pointers to nodes
+        std::size_t _capacity;  // size of array
+
+        inline void swap(std::size_t i, std::size_t j){
+            assert(i >= 0 && i < size);
+            assert(j >= 0 && j < size);
+            HeapNode<Node_t, Compare> * xi = _data[i].load(std::memory_order_relaxed);
+            HeapNode<Node_t, Compare> * xj = _data[j].load(std::memory_order_relaxed);
+            swap_helper(i, j, xi, xj, _data);
+        }
+
+        inline void pull_up(handle_t i){
+            handle_t j = parent(i); 
+            HeapNode<Node_t, Compare> * xi = _data[i].load(std::memory_order_relaxed);
+            HeapNode<Node_t, Compare> * xj = _data[j].load(std::memory_order_relaxed);
+            if(i == 0 || Compare()(xj->search_node, xi->Search_node)){
+                return;
+            }
+            swap_helper(i, j, xi, xj, _data);
+            pull_up(j);
+        }
+
+        inline void push_down(handle_t i, const std::size_t s){
+            handle_t l = left_child(i);
+            handle_t r = right_child(i); 
+            HeapNode<Node_t, Compare> * xi = _data[i].load(std::memory_order_relaxed);
+            handle_t smallest_i = i;
+            HeapNode<Node_t, Compare> * smallest = xi;
+            if(l < s){
+                HeapNode<Node_t, Compare> * xl = arr[l].load(std::memory_order_relaxed);
+                if(Compare()(xl->search_node, smallest->Search_node)){
+                    smallest = xl;
+                    smallest_i = l;
+                }
+                if(r < s){
+                    HeapNode<Node_t, Compare> * xr = arr[l].load(std::memory_order_relaxed);
+                    if(Compare()(xr->search_node, smallest->Search_node)){
+                        smallest = xr;
+                        smallest_i = r;
+                    }
+                }
+            }
+            if(smallest != xi){
+                swap_helper(i, smallest_i, xi, smallest, arr);
+                push_down(smallest_i, s);
+            }
+        }
+
+        // inline resize(std::size_t new_capacity){
+        //     assert(new_capacity >= size);
+        //     auto new_data = std::shared_ptr<Flagged_Pointer<HeapNode>>(new Flagged_Pointer<HeapNode>[new_size], std::default_delete<Flagged_Pointer<HeapNode>>());
+        //     for (std::size_t i = 0; i < size; i++){
+        //         Flagged_Pointer<HeapNode>::copy_data(_data.get()[i], new_data.get()[i]);
+        //     }
+        //     _capacity = new_capacity;
+        //     _data.store(new_data, std::memory_order_release);
+        // }
+
     public:
-        inline Node_t * peek() const{
-            return _head_queue.peek().get_pointer();
+        std::atomic<std::size_t> size;       // size of heap        
+
+        inline CafeMinBinaryHeap(std::size_t capacity, std::size_t max_nodes):_capacity(capacity),_max_nodes(max_nodes){
+            // setup heap before workers!
+            _data = new std::atomic<HeapNode<Node_t, Compare> *>[capacity];
+            size.store(0, std::memory_order_release);
+        }
+
+        inline HeapNode top() const{
+            assert(size > 0);
+            return *(_data.load(std::memory_order_acquire)->get_pointer());
         }
 
         inline void pop(){
-            _head_queue.pop();
-            _head_queue.push(_tail_queue.top());
-            _tail_queue.pop();
-        }        
+            std::size_t s = size.load(std::memory_order_relaxed);
+            swap(--s, 0);
 
-        inline void push(Node_t * x){
-            Flagged_Pointer<Node_t> fx(x);
-            if(_head_queue.length < _head_queue._capacity){
-                _head_queue.push(fx);
-            }
-            else if(flagged_ptr_cmp<Node_t *, Compare>(_head_queue.peek_tail(), fx)){
-                auto holder = _head_queue.push(fx);
-                _tail_queue.push(holder);
-            }
-            else{
-                _tail_queue.push(holder);
-            }
+            size.store(s, std::memory_order_release); 
         }
+
+        inline void push(HeapNode * node){
+            std::size_t s = size.load(std::memory_order_relaxed);
+            _data[s].store(node, std::memory_order_relaxed);
+            pull_up(s, _data);
+            size.store(++s, std::memory_order_release); 
+        }
+
 };
--- Cafe_Heap/flagged_ptr.hpp
+++ Cafe_Heap/flagged_ptr.hpp
@@ -12,24 +12,46 @@ class Flagged_Pointer{
         constexpr static uint64_t expanded_bit = 0x1;   
     public:
         constexpr Flagged_Pointer(ptr_t pointer){
-            _data = (uintptr_t)pointer; // cast to int type
+            _data.store((uintptr_t)pointer, // cast to int type
+                        std::memory_order_release); // publish to others
             assert((_data & 0x3) == 0x0); // double check that bottom bits are free for flags
+        }
+  
+
+
+        friend constexpr void copy_data(const Flagged_Pointer<ptr_t>& from, Flagged_Pointer<ptr_t>& to){
+            to._data = from._data.load(std::memory_order_acquire);
+        }
+
+        friend constexpr void swap(Flagged_Pointer<ptr_t>& left, Flagged_Pointer<ptr_t>& right) const{
+            left._data.store(right._data.exchange(left._data, std::memory_order_relaxed), 
+                                std::memory_order_relaxed);
         }
 
         constexpr ptr_t get_pointer() const{
             return (ptr_t)(pointer_mask & _data);
         }
 
-        constexpr bool claim(){
-            return _data.fetch_or(working_bit) & working_bit;
+        constexpr ~Flagged_Pointer(){
+            auto ptr = get_pointer();
+            if(ptr != nullptr){
+                delete *ptr;
+            }
         }
 
-        constexpr bool finalize(){
-            return _data.fetch_or(expanded_bit) & expanded_bit;
+        constexpr bool worker_claim(){
+            return working_bit & _data.fetch_or(working_bit,  // try and claim, check if available
+                                  std::memory_order_acquire); // pull from others  NOTE, we only need atomic for other workers, 
+                                                              // but acquire is so that the main thread can gate the node creation behind a release
         }
 
-        constexpr bool expanded() const{
-            return _data.load() & expanded_bit;
+        constexpr bool worker_finalize(){
+            return expanded_bit & _data.fetch_or(expanded_bit,  // set expansion is precomputed 
+                                    std::memory_order_release); // push info to main thread
+        }
+
+        constexpr bool isExpanded() const{
+            return expanded_bit & _data.load(std::memory_order_acquire);  // Check if expansion has been computed, pulling info first
         }  
 };
 
