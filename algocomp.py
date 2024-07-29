9f018697e5b4bc8731a6800b5de3260b0ed989d9
autumnnippert <chrisnippert22@gmail.com>
2024-07-29 16:53:10 -0400

    .py - algocomp update, readme.md, fedora packages update, added TODO

--- algocomp.py
+++ algocomp.py
@@ -7,8 +7,12 @@ import os
 
 import json
 
+# get date time
+from datetime import datetime
+now = datetime.now()
+
 class AlgoResult:
-    def __init__(self, file, wall_start_date, wall_start_time, machine_id, cost, initial_heuristic, initial_distance, algorithm, final_sol_cost, state_size, packed_state_size, final_sol_length, total_raw_cpu_time, total_wall_time, total_nodes_expanded, total_nodes_generated, total_nodes_duplicated, total_nodes_reopened, closed_list_type, closed_fill, closed_collisions, closed_resizes, closed_buckets, closed_max_bucket_fill, open_list_type, node_size, wall_finish_time, wall_finish_date, max_virtual_kilobytes):
+    def __init__(self, file, wall_start_date, wall_start_time, machine_id, cost, initial_heuristic, initial_distance, algorithm, final_sol_cost, state_size, packed_state_size, final_sol_length, total_raw_cpu_time, total_wall_time, total_nodes_expanded, total_nodes_generated, total_nodes_duplicated, total_nodes_reopened, closed_list_type, closed_fill, closed_collisions, closed_resizes, closed_buckets, closed_max_bucket_fill, open_list_type, node_size, wall_finish_time, wall_finish_date, max_virtual_kilobytes, threads = 0):
         self.file = file
         self.wall_start_date = wall_start_date
         self.wall_start_time = wall_start_time
@@ -38,6 +42,7 @@ class AlgoResult:
         self.wall_finish_time = wall_finish_time
         self.wall_finish_date = wall_finish_date
         self.max_virtual_kilobytes = max_virtual_kilobytes
+        self.threads = threads
 
     def __str__(self):
         return f"file: {self.file}\nwall_start_date: {self.wall_start_date}\nwall_start_time: {self.wall_start_time}\nmachine_id: {self.machine_id}\ncost: {self.cost}\ninitial_heuristic: {self.initial_heuristic}\ninitial_distance: {self.initial_distance}\nalgorithm: {self.algorithm}\nfinal_sol_cost: {self.final_sol_cost}\nstate_size: {self.state_size}\npacked_state_size: {self.packed_state_size}\nfinal_sol_length: {self.final_sol_length}\ntotal_raw_cpu_time: {self.total_raw_cpu_time}\ntotal_wall_time: {self.total_wall_time}\ntotal_nodes_expanded: {self.total_nodes_expanded}\ntotal_nodes_generated: {self.total_nodes_generated}\ntotal_nodes_duplicated: {self.total_nodes_duplicated}\ntotal_nodes_reopened: {self.total_nodes_reopened}\nclosed_list_type: {self.closed_list_type}\nclosed_fill: {self.closed_fill}\nclosed_collisions: {self.closed_collisions}\nclosed_resizes: {self.closed_resizes}\nclosed_buckets: {self.closed_buckets}\nclosed_max_bucket_fill: {self.closed_max_bucket_fill}\nopen_list_type: {self.open_list_type}\nnode_size: {self.node_size}\nwall_finish_time: {self.wall_finish_time}\nwall_finish_date: {self.wall_finish_date}\nmax_virtual_kilobytes: {self.max_virtual_kilobytes}"
@@ -59,7 +64,7 @@ class AlgoResult:
         """
         output = output.split("\n")
         output = output[1:-2]
-        wall_start_date = wall_start_time = machine_id = cost = initial_heuristic = initial_distance = algorithm = final_sol_cost = state_size = packed_state_size = final_sol_length = total_raw_cpu_time = total_wall_time = total_nodes_expanded = total_nodes_generated = total_nodes_duplicated = total_nodes_reopened = closed_list_type = closed_fill = closed_collisions = closed_resizes = closed_buckets = closed_max_bucket_fill = open_list_type = node_size = wall_finish_time = wall_finish_date = max_virtual_kilobytes = None
+        wall_start_date = wall_start_time = machine_id = cost = initial_heuristic = initial_distance = algorithm = final_sol_cost = state_size = packed_state_size = final_sol_length = total_raw_cpu_time = total_wall_time = total_nodes_expanded = total_nodes_generated = total_nodes_duplicated = total_nodes_reopened = closed_list_type = closed_fill = closed_collisions = closed_resizes = closed_buckets = closed_max_bucket_fill = open_list_type = node_size = wall_finish_time = wall_finish_date = max_virtual_kilobytes = threads = None
         for line in output:
             arr = line.split("\t")
             key = arr[0][8:-1]
@@ -122,10 +127,14 @@ class AlgoResult:
                 wall_finish_date = val
             elif key == "max virtual kilobytes":
                 max_virtual_kilobytes = val
-        return AlgoResult(file, wall_start_date, wall_start_time, machine_id, cost, initial_heuristic, initial_distance, algorithm, final_sol_cost, state_size, packed_state_size, final_sol_length, total_raw_cpu_time, total_wall_time, total_nodes_expanded, total_nodes_generated, total_nodes_duplicated, total_nodes_reopened, closed_list_type, closed_fill, closed_collisions, closed_resizes, closed_buckets, closed_max_bucket_fill, open_list_type, node_size, wall_finish_time, wall_finish_date, max_virtual_kilobytes)
+            elif key == "threads":
+                threads = val
+        if threads == None:
+            threads = 0
+        return AlgoResult(file, wall_start_date, wall_start_time, machine_id, cost, initial_heuristic, initial_distance, algorithm, final_sol_cost, state_size, packed_state_size, final_sol_length, total_raw_cpu_time, total_wall_time, total_nodes_expanded, total_nodes_generated, total_nodes_duplicated, total_nodes_reopened, closed_list_type, closed_fill, closed_collisions, closed_resizes, closed_buckets, closed_max_bucket_fill, open_list_type, node_size, wall_finish_time, wall_finish_date, max_virtual_kilobytes, threads)
 
 if len(sys.argv) <= 3:
-    print("Usage: python3 korfcomp.py <path to executable> <path to folder> [algos...]")
+    print("Usage: python3 algocomp.py <path to executable> <path to folder> [algo1 -args:algo2 -args...]")
     exit(1)
 
 runner = sys.argv[1]
@@ -142,6 +151,11 @@ if not os.path.isfile(runner):
 
 if not os.path.exists("./algocomp_res"):
     os.makedirs("./algocomp_res")
+
+# put algo_list into a string
+algo_list = " ".join(algo_list)
+# parse by ':'
+algo_list = algo_list.split(":")
 
 print(f"Running: {runner}")
 print(f"Folder: {folder}")
@@ -180,6 +194,8 @@ for file in lst: # non inclusive
     print(f"{folder}{file} --------------------")
 
     for algo in algo_list:
+        algokey = '-'.join(algo.split(" "))
+        algokey = algokey.replace(" ", "")
         command = f"{runner} {algo} < {folder}/{file} > tmp"
         output = result = subprocess.check_output(command, shell=True)
         f = open("tmp", "r")
@@ -190,23 +206,32 @@ for file in lst: # non inclusive
             continue
         print(f"{algo}: {cpu_time}")
         if algo not in cumulative_times:
-            cumulative_times[algo] = 0
-        cumulative_times[algo] += cpu_time
+            cumulative_times[algokey] = 0
+        cumulative_times[algokey] += cpu_time
         if algo not in algo_json_results:
-            algo_json_results[algo] = []
-        algo_json_results[algo].append(AlgoResult.from_output(file, output)) # this * thing is kinda cool
+            algo_json_results[algokey] = []
+        algo_json_results[algokey].append(AlgoResult.from_output(file, output)) # this * thing is kinda cool
+        # if -threads k is found, set algo's threads to k
+        if "-threads" in algo:
+            algo_json_results[algokey][-1].threads = int(algo.split(" ")[-1])
+        f.close()
+    
+    
+    filename = f'./algocomp_res/{file}-{now}.json'
+    filename = filename.replace(":", "-") # Clean up for windows
+
+    # append to json file for each file
+    with open(f"{filename}", "w") as f:
+        f.write(json.dumps([x.__dict__ for x in algo_json_results[algo]]))
         f.close()
 
 print("==========Final Stats==========")
 for algo in algo_list:
     print(f"{algo: <10}: {cumulative_times[algo]}")
 
-# get date time
-from datetime import datetime
-now = datetime.now()
 
-for algo in algo_json_results:
-    # write to json file
-    with open(f"./algocomp_res/{algo}-{now}.json", "w") as f:
-        f.write(json.dumps([x.__dict__ for x in algo_json_results[algo]]))
-        f.close()
+# for algo in algo_json_results:
+#     # write to json file
+#     with open(f"./algocomp_res/{algo}-{now}.json", "w") as f:
+#         f.write(json.dumps([x.__dict__ for x in algo_json_results[algo]]))
+#         f.close()
